8b84342dbf23c84eac749f164f10100f7db9a4a4
anajufg <anajufg127@gmail.com>
2025-05-27 18:03:26 -0300

    resolving conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/lireo">
     <App />
   </BrowserRouter>
 );
