33285728f9c542d93711d808bc7e0ea5e5bafbd0
anajufg <anajufg127@gmail.com>
2025-06-09 10:45:20 -0300

    #33 fixing responsiviness

--- src/features/AboutUs/components/Section.tsx
+++ src/features/AboutUs/components/Section.tsx
@@ -4,7 +4,13 @@ import { getPublicAssetUrl } from "../../../utils/pathUtils";
 
 interface SectionProps {
   title: string;
-  members: { name: string; paragraph1: string; paragraph2: string; paragraph3: string; photo: string }[];
+  members: {
+    name: string;
+    paragraph1: string;
+    paragraph2: string;
+    paragraph3: string;
+    photo: string;
+  }[];
 }
 
 const Section: React.FC<SectionProps> = ({ title, members }) => {
