d0a33230436f83ef02838b1cca6632bfe775c14b
isinharamox <162699578+isinharamox@users.noreply.github.com>
2025-10-06 18:29:18 -0300

    Feature/#52 about us page update
    * #51 Refactor code style across multiple files to use single quotes and improve CSS formatting
    
    - Updated string literals to use single quotes in JavaScript and TypeScript files.
    - Reformatted CSS transition properties for better readability.
    - Adjusted import statements to maintain consistency in quotation marks.
    - Enhanced user guide text for clarity and consistency.
    - Improved loading and fade-in components for better code organization.
    - Added Prettier configuration for consistent code formatting.
    
    * #51 chore: update package.json to add prettier and eslint plugins; enhance code quality tools
    
    fix: add missing React import in Header component
    
    fix: improve error handling in useTextReader hook and update imports
    
    fix: update dependency array in useWordHighlighter hook for better performance
    
    fix: add missing React import in FadeIn component
    
    * #51 refactor: clean up eslint config and remove unnecessary whitespace; improve code readability
    
    * #52 Refactor code structure for improved readability and maintainability and add collaborators.

--- src/features/AboutUs/text/textAboutUs.ts
+++ src/features/AboutUs/text/textAboutUs.ts
@@ -29,27 +29,58 @@ export const projectCreators: Member[] = [
     affiliation: ' COLTEC/UFMG',
     photo: '/images/AnaJulia_TeamMemberPicture.png',
   },
-  {
-    type: 'teamMember',
-    name: 'Raphael Inácio Bicalho de Carvalho',
-    role: 'Desenvolvedor',
-    position: 'Bolsista de Iniciação Científica',
-    education: 'Técnico em Desenvolvimento de Sistemas',
-    affiliation: ' COLTEC/UFMG',
-    photo: '/images/Raphael_TeamMember.png',
-  },
+
+];
+
+export const projectCollaborators: Member[] = [
   {
     type: 'teamMember',
     name: 'Isadora Ramos Pereira',
-    role: 'Desenvolvedora',
+    role: 'Colaboradora',
     position: 'Bolsista de Iniciação Científica',
     education: 'Técnico em Desenvolvimento de Sistemas',
     affiliation: ' COLTEC/UFMG',
     photo: '/images/Isadora_TeamMemberPicture.jpg',
   },
+  {
+    type: 'teamMember',
+    name: 'Thiago Calebe Ramos dos Santos',
+    role: 'Colaborador',
+    position: 'Bolsista de Iniciação Científica',
+    education: 'Técnico em Desenvolvimento de Sistemas',
+    affiliation: ' COLTEC/UFMG',
+    photo: '/images/Thiago_TeamMemberPicture.jpg',
+  },
+  {
+    type: 'teamMember',
+    name: 'Izabelly Monteiro',
+    role: 'Colaboradora',
+    position: 'Docente de Língua Portuguesa',
+    education: 'Licenciada em Letras',
+    affiliation: 'Faculdade de Letras/UFMG',
+    photo: '/images/Izabelly_TeamMemberPicture.jpg',
+  },
+  {
+    type: 'teamMember',
+    name: 'Debora Fraga Lodi',
+    role: 'Colaboradora',
+    position: 'Departamento de Fonoaudiologia',
+    education: 'Faculdade de Medicina/UFMG',
+    affiliation: 'CRFa 6-5743',
+    photo: '/images/Debora_TeamMemberPicture.jpg',
+  },
+  {
+    type: 'teamMember',
+    name: 'Débora Marques de Miranda',
+    role: 'Colaboradora',
+    position: 'Professora Associada',
+    education: 'Coordenadora do Centro de Tecnologia da Faculdade de Medicina',
+    affiliation: 'Departamento de Pediatria/UFMG',
+    photo: '/images/DéboraMarques_TeamMemberPicture.jpg',
+  },
 ];
 
-export const collaborators: Member[] = [
+export const projectExecution: Member[] = [
   {
     type: 'collaborator',
     photo: '/images/logo-Faculdade-de-Medicina-UFMG.png',
@@ -60,7 +91,13 @@ export const collaborators: Member[] = [
     photo: '/images/logo-UFMG.png',
     alt: 'UFMG',
   },
+  {
+    type: 'collaborator',
+    photo: '/images/logo-Neurotec-INCT .png',
+    alt: 'INCT',
+  },
 ];
+
 
 export const aboutUsText = {
   summary: {
@@ -94,4 +131,5 @@ export const aboutUsText = {
   },
   creatorsSection: 'Idealizadores do Projeto',
   collaboratorsSection: 'Colaboradores do Projeto',
+  executionSection: 'Realização',
 };
